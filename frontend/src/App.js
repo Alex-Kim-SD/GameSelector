e7e882aaf38d87b17d94b86d9fb20e2b488ca24c
alex5 <alex@kimnetics.com>
2023-09-20 19:14:48 -1000

    bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { Switch, Route, useLocation, Redirect } from 'react-router-dom';
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingPage';
 
 function App() {
   return (
     <div className='App'>
-      <Switch>
-        {/* <Route path='/login'>
-          <LoginFormPage />
-        </Route>
-        <Route path='/signup'>
-          <SignupFormPage />
-        </Route> */}
-        <Route path='/'>
-          <LandingPage />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
