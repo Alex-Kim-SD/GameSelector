2e927ec5499552d18640db4d67224e537b9dffcc
Syrr <Parker.Brockway@gmail.com>
2023-09-20 22:10:58 -0700

    bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, useLocation, Redirect } from 'react-router-dom';
+
+import LandingPage from './components/landingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        {/* <Route path='/login'>
+          <LoginFormPage />
+        </Route>
+        <Route path='/signup'>
+          <SignupFormPage />
+        </Route> */}
+        <Route path='/'>
+          <LandingPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
